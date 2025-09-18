31063624841c08a8702920ed6b06fe1873b9c722
Behrooz Vedadian <vedadian@gmail.com>
2025-09-18 11:45:07 +0330

    feat: Refactor Contributor component and update QuestPage
    The Contributor component has been completely refactored into QuestContributorView, which now includes a detailed layout for displaying quest information, images, and user interactions. The component imports various UI elements such as Carousel, Avatar, and ReflowModal to enhance functionality and user experience.
    
    Additionally, the QuestPage has been updated to render the new QuestContributorView instead of the previous Contributor component, ensuring that the latest design and features are utilized.
    
    No breaking changes introduced.

--- src/app/quest/[questId]/page.tsx
+++ src/app/quest/[questId]/page.tsx
@@ -1,6 +1,6 @@
 import { Sidebar } from "@/components/sidebar";
 import { QuestStarterView } from "./starter";
-import { MobileHeader } from "@/components/mobile_header";
+import { QuestContributorView } from "./contributor";
 
 export default async function QuestPage() {
   return (
@@ -8,7 +8,7 @@ export default async function QuestPage() {
       <Sidebar />
       <div className="flex-1 bg-base-200 relative overflow-scroll">
         <div className="absolute top-0 left-0 w-full flex flex-col items-center justify-center">
-          <QuestStarterView />
+          <QuestContributorView />
         </div>
       </div>
     </div>
