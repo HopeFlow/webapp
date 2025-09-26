39e449f075919d45ba969afe4eb1a713652202bf
Saeed <saeed.torabzadeh@gmail.com>
2025-09-26 12:59:38 +0200

    Implements user profile management with image upload
    Adds functionality to manage user profiles, including the ability to upload and preview profile images. Introduces hooks for handling user data and integrating with the backend for profile updates.
    
    Removes the old profile creation page and refactors the profile component to utilize the new image picker and loading states. Enhances user experience by showing loading indicators during image fetching and submission.
    
    Implements safe user data handling and ensures proper cleanup of image URLs to prevent memory leaks.
    
    Fixes various types and state handling for better user input management.

--- src/app/profile/[[...command]]/page.tsx
+++ src/app/profile/[[...command]]/page.tsx
@@ -1,10 +1,13 @@
 import { z } from "zod";
-import { withParams } from "@/helpers/server/with_params";
 import { ProfileMain } from "./main";
+import { withParamsAndUser } from "@/helpers/server/page_component";
+import { user2SafeUser } from "@/helpers/server/auth";
 
-export default withParams(
-  function Profile({ command, url }) {
-    return <ProfileMain command={command?.[0]} url={url} />;
+export default withParamsAndUser(
+  function Profile({ command, url, user }) {
+    if (!user) return null;
+    const safeUser = user2SafeUser(user);
+    return <ProfileMain command={command?.[0]} url={url} user={safeUser} />;
   },
   {
     paramsTypeDef: z.object({
