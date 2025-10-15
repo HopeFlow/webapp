9e317f84026ba358f3b54de8ae0a617213931b0f
Behrooz Vedadian <vedadian@gmail.com>
2025-10-15 16:20:16 +0330

    fix: Update ESLint and Next.js configuration
    Updated the ESLint configuration to improve readability and maintainability. The `FlatCompat` instantiation has been streamlined, and the file ignores have been organized for clarity. Additionally, the `files` property in the ESLint config now includes all relevant file types in the `src/server_actions/client` directory.
    
    In the Next.js configuration, commented-out ESLint disable directives have been converted to single-line comments for consistency and to avoid potential confusion.
    
    No breaking changes.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,19 +5,29 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [{
-  ignores: [".wrangler/**", "node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}, {
-  ignores: ["src/3rdparty/**", "scripts/**"],
-}, ...compat.extends("next/core-web-vitals", "next/typescript"), {
-  files: ["src/server_actions/client/create_account/**"],
-  rules: {
-    "eslint-comments/no-unused-disable": "off"
-  }
-}];
+const eslintConfig = [
+  {
+    ignores: [
+      ".wrangler/**",
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "cloudflare-env.d.ts",
+      "next-env.d.ts",
+    ],
+  },
+  { ignores: ["src/3rdparty/**", "scripts/**"] },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["src/server_actions/client/**/*.{js,ts,jsx,tsx}"],
+    linterOptions: {
+      reportUnusedDisableDirectives: "off",
+    },
+    rules: { "eslint-comments/no-unused-disable": "off" },
+  },
+];
 
 export default eslintConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
+// /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+// /* eslint-disable @typescript-eslint/no-unsafe-return */
 
 import createWithAnalyser from "@next/bundle-analyzer";
 
