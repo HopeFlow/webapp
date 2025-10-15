5a2e704369b788b5c21fb5fc696b5f43f565706f
Behrooz Vedadian <vedadian@gmail.com>
2025-10-15 11:15:20 +0330

    feat: Refactor Sidebar component for improved navigation
    The Sidebar component has been refactored to enhance its structure and functionality. The following changes were made:
    - Introduced a new type `SidebarItem` to define the structure of sidebar items.
    - Replaced static button elements with a dynamic mapping of `SidebarItem` objects, allowing for easier management of sidebar items.
    - Added navigation functions for each sidebar item, enabling users to navigate to different sections of the application.
    - Improved accessibility by adding `aria-disabled` attributes to disabled items.
    
    No breaking changes introduced.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -10,43 +10,115 @@ import { TrophyIcon } from "./icons/trophy";
 import { UserIcon } from "./icons/user";
 import { UserAvatarAndMenu } from "./useravatar_menu";
 import type { SafeUser } from "@/helpers/server/auth";
+import {
+  useGotoCreateQuest,
+  useGotoHome,
+  useGotoNotifications,
+  useGotoProfile,
+  useGotoTrophies,
+} from "@/helpers/client/routes";
+import { ReactNode, useMemo } from "react";
 
-export const Sidebar = ({ user }: { user: SafeUser }) => (
-  <div className="w-60 h-auto p-6 hidden md:flex flex-col items-center border-r-2 bg-base-100 border-base-300">
-    <Image
-      src="/img/wordmark.svg"
-      className="max-w-full h-auto object-contain"
-      alt="Home"
-      width={118}
-      height={37}
-    />
-    <div className="w-full h-12"></div>
-    <div className="w-full flex flex-col gap-4 [&>*]:w-full [&>button]:text-lg">
-      <GhostButton className="font-thin flex flex-row gap-2 justify-start">
-        <HomeIcon /> Home
-      </GhostButton>
-      <GhostButton className="font-thin flex flex-row gap-2 justify-start">
-        <BellIcon /> Notifications
-      </GhostButton>
-      <GhostButton className="font-thin flex flex-row gap-2 justify-start">
-        <ChatBubbleIcon /> Chat
-      </GhostButton>
-      <GhostButton className="font-thin flex flex-row gap-2 justify-start">
-        <TrophyIcon /> Trophies
-      </GhostButton>
-      <GhostButton className="font-thin flex flex-row gap-2 justify-start">
-        <UserIcon /> Profile
-      </GhostButton>
-      <GhostButton
-        className="font-thin flex flex-row gap-2 justify-start"
-        onClick={() => {}}
-      >
-        <PlusIcon /> Create a quest
-      </GhostButton>
+type SidebarItem = {
+  id: string;
+  content: ReactNode;
+  goto?: () => void; // optional so we can disable gracefully
+  disabled?: boolean;
+};
+
+export const Sidebar = ({ user }: { user: SafeUser }) => {
+  const gotoHome = useGotoHome();
+  const gotoNotifications = useGotoNotifications();
+  const gotoTrophies = useGotoTrophies();
+  const gotoProfile = useGotoProfile();
+  const gotoCreateQuest = useGotoCreateQuest();
+
+  const items = useMemo<SidebarItem[]>(
+    () => [
+      {
+        id: "home",
+        content: (
+          <>
+            <HomeIcon /> Home
+          </>
+        ),
+        goto: gotoHome,
+      },
+      {
+        id: "notifications",
+        content: (
+          <>
+            <BellIcon /> Notifications
+          </>
+        ),
+        goto: gotoNotifications,
+      },
+      {
+        id: "messages",
+        content: (
+          <>
+            <ChatBubbleIcon /> Messages
+          </>
+        ),
+        disabled: true,
+      },
+      {
+        id: "trophies",
+        content: (
+          <>
+            <TrophyIcon /> Trophies
+          </>
+        ),
+        goto: gotoTrophies,
+      },
+      {
+        id: "profile",
+        content: (
+          <>
+            <UserIcon /> Profile
+          </>
+        ),
+        goto: gotoProfile,
+      },
+      {
+        id: "create",
+        content: (
+          <>
+            <PlusIcon /> Create Quest
+          </>
+        ),
+        goto: gotoCreateQuest,
+      },
+    ],
+    [gotoHome, gotoNotifications, gotoTrophies, gotoProfile, gotoCreateQuest],
+  );
+  return (
+    <div className="bg-base-100 border-base-300 hidden h-auto w-60 flex-col items-center border-r-2 p-6 md:flex">
+      <Image
+        src="/img/wordmark.svg"
+        className="h-auto max-w-full object-contain"
+        alt="HopeFlow"
+        width={118}
+        height={37}
+      />
+      <div className="h-12 w-full"></div>
+      <div className="flex w-full flex-col gap-4 [&>*]:w-full [&>button]:text-lg">
+        {items.map((item, index) => (
+          <GhostButton
+            key={`sidebar-item-${index}`}
+            className="flex flex-row justify-start gap-2 font-thin"
+            onClick={item.disabled ? undefined : item.goto}
+            disabled={item.disabled}
+            aria-disabled={item.disabled}
+          >
+            {item.content}
+          </GhostButton>
+        ))}
+      </div>
+      <div className="w-full flex-1"></div>
+      <div className="flex w-full flex-col gap-4 pl-4 [&>*]:w-full">
+        <UserAvatarAndMenu user={user} />
+      </div>
     </div>
-    <div className="w-full flex-1"></div>
-    <div className="w-full flex flex-col gap-4 [&>*]:w-full pl-4">
-      <UserAvatarAndMenu user={user} />
-    </div>
-  </div>
-);
+  );
+};
