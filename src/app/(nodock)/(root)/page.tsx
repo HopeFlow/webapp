c2cb1ba54184e047a5517a280cc33b41327aad9e
Behrooz Vedadian <vedadian@gmail.com>
2025-10-16 15:16:25 +0330

    fix: Clean up imports and adjust indentation in RootPage component
    Removed duplicate imports for publicPage and redirectToHome in the RootPage component. Adjusted the indentation of the database initialization line for better readability. This change improves code clarity and maintains consistency in formatting.
    
    No breaking changes.

--- src/app/(nodock)/(root)/page.tsx
+++ src/app/(nodock)/(root)/page.tsx
@@ -3,13 +3,11 @@ import Onboarding from "./onboarding";
 import { questTable } from "@/db/schema";
 import { publicPage, withUser } from "@/helpers/server/page_component";
 import { redirectToHome } from "@/helpers/server/routes";
-import { publicPage, withUser } from "@/helpers/server/page_component";
-import { redirectToHome } from "@/helpers/server/routes";
 
 export default publicPage(
   withUser(async function RootPage({ user }) {
     // TODO: Remove these extra parts required to ensure DB file is created, ASAP
-  const db = await getHopeflowDatabase();
+    const db = await getHopeflowDatabase();
     const q = await db.select().from(questTable).limit(1);
     console.log(q);
     if (user) redirectToHome();
