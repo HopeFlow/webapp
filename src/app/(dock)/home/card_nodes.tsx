fab3c90e61bd5fcb02dac5a01100ae89e779bce0
Saeed <saeed.torabzadeh@gmail.com>
2025-10-20 14:02:01 +0200

    refactor: Clean up UserAvatarProps type definition and improve Avatar component styling

--- src/app/(dock)/home/card_nodes.tsx
+++ src/app/(dock)/home/card_nodes.tsx
@@ -14,7 +14,7 @@ export const StarterQuestCardNodes = ({
   }>;
 }) => {
   return (
-    <div className="relative flex flex-col self-stretch items-center justify-between">
+    <div className="relative flex flex-col items-center justify-between self-stretch">
       <div className="flex flex-col items-center text-neutral-500">
         <ShareIcon />
         <h2>{nodes.length - 1}</h2>
@@ -22,20 +22,20 @@ export const StarterQuestCardNodes = ({
       <div className="h-4"></div>
       {nodes.length > 1 && (
         <>
-          <div className="flex flex-row text-xs items-center justify-center">
+          <div className="flex flex-row items-center justify-center text-xs">
             {elapsedTime2String(nodes[nodes.length - 1].activityDate)}
           </div>
           <Avatar
-            className="w-6 md:w-9 border-1 border-primary rounded-full"
+            className="border-primary w-6 rounded-full border-1 md:w-9"
             {...nodes[nodes.length - 1]}
           />
         </>
       )}
       {nodes.length > 1 && (
-        <hr className="flex-1 w-0 border-primary border-1" />
+        <hr className="border-primary w-0 flex-1 border-1" />
       )}
       {nodes.length > 5 && (
-        <hr className="flex-1 w-0 border-primary border-1 border-dashed" />
+        <hr className="border-primary w-0 flex-1 border-1 border-dashed" />
       )}
       {nodes.length > 2 && (
         <>
@@ -43,16 +43,16 @@ export const StarterQuestCardNodes = ({
             {nodes.slice(-4, -1).map((avatar, i) => (
               <Avatar
                 key={`avatar-${i + 1}`}
-                className="w-6 md:w-9 border-1 border-primary rounded-full"
+                className="border-primary w-6 rounded-full border-1 md:w-9"
                 {...avatar}
               />
             ))}
           </div>
-          <hr className="flex-1 w-0.5 bg-primary border-none" />
+          <hr className="bg-primary w-0.5 flex-1 border-none" />
         </>
       )}
       <Avatar
-        className="w-8 md:w-12 border-3 border-primary rounded-full"
+        className="border-primary w-8 rounded-full border-3 md:w-12"
         {...nodes[0]}
       />
       <div className="absolute bottom-0 left-[calc(100%+0.5rem)] w-auto text-sm whitespace-nowrap">
@@ -75,24 +75,24 @@ export const ContributorQuestCardNodes = ({
   }>;
 }) => {
   return (
-    <div className="relative flex flex-col self-stretch items-center justify-between">
+    <div className="relative flex flex-col items-center justify-between self-stretch">
       {nodes.length > 1 && (
         <Avatar
-          className="w-8 md:w-12 border-3 border-primary rounded-full"
+          className="border-primary w-8 rounded-full border-3 md:w-12"
           {...nodes[nodes.length - 1]}
         />
       )}
       {nodes.length > 1 && (
-        <hr className="flex-1 w-0 border-primary border-1" />
+        <hr className="border-primary w-0 flex-1 border-1" />
       )}
       {nodes.length > 2 && (
         <>
-          <hr className="flex-4 w-0 border-primary border-1 border-dashed" />
-          <hr className="flex-1 w-0 border-primary border-1" />
+          <hr className="border-primary w-0 flex-4 border-1 border-dashed" />
+          <hr className="border-primary w-0 flex-1 border-1" />
         </>
       )}
       <Avatar
-        className="w-6 md:w-8 border-1 border-primary rounded-full"
+        className="border-primary w-6 rounded-full border-1 md:w-8"
         {...nodes[0]}
       />
       <div className="absolute bottom-0 left-[calc(100%+0.5rem)] w-auto text-sm whitespace-nowrap">
