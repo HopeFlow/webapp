b5cd37d4b0f1707dd858cc337b2c365e9fa24797
Behrooz Vedadian <vedadian@gmail.com>
2025-11-04 00:05:35 +0330

    feat: Refactor Steps component for improved visibility logic
    The Steps component has been refactored to enhance the logic for determining visible steps based on the total number of steps and the current step. The condition for displaying steps has been updated to ensure that it correctly handles cases where the number of steps is less than or equal to the maximum visible steps.
    
    Additionally, the calculation for the 'from' and 'to' indices has been streamlined to improve readability and maintainability. Console logs have been retained for debugging purposes.
    
    No breaking changes introduced.

--- src/components/steps.tsx
+++ src/components/steps.tsx
@@ -7,22 +7,43 @@ export type StepsProps = {
   onClick?: (step: number) => void;
 };
 
+const maxVisibleSteps = 7;
+
 export function Steps({ onClick, numberOfSteps, currentStep }: StepsProps) {
   const visibleSteps = (() => {
-    if (numberOfSteps < 7)
+    if (numberOfSteps <= maxVisibleSteps)
       return new Array(numberOfSteps).fill(null).map((_, i) => i);
-    const [from, to] = ((s, e) => {
-      const from = Math.max(0, Math.min(e - 5, s));
-      return [from, e];
-    })(
-      currentStep > 2 ? currentStep - 1 : 0,
-      currentStep < numberOfSteps - 4 ? currentStep + 2 : numberOfSteps,
-    );
+    const [from, to] = (() => {
+      const h0 = Math.ceil((maxVisibleSteps - 5) / 2);
+      const h1 = maxVisibleSteps - 5;
+      let from = 0;
+      let to = numberOfSteps;
+      if (2 * currentStep <= numberOfSteps) {
+        from = Math.max(0, currentStep - h0);
+        to = Math.min(numberOfSteps, from + h1 + 1 + Math.max(0, 2 - from));
+      } else {
+        to = Math.min(numberOfSteps, currentStep + h0 + 1);
+        from = Math.max(0, to - h1 - 1 - Math.max(0, to - numberOfSteps + 2));
+      }
+      return [
+        from <= 2 ? 0 : from,
+        to >= numberOfSteps - 2 ? numberOfSteps : to,
+      ];
+    })();
     console.log({ from, to, currentStep, numberOfSteps });
     const result = [
-      ...(from === 0 ? [] : [0, -1]),
-      ...new Array(to - from).fill(null).map((_, i) => from + i),
-      ...(to === numberOfSteps ? [] : [-1, numberOfSteps - 1]),
+      ...(from === 0 ? [] : from === 1 ? [0] : [0, -1]),
+      ...new Array(
+        (to === numberOfSteps - 1 ? numberOfSteps : to) -
+          (from === 1 ? 0 : from),
+      )
+        .fill(null)
+        .map((_, i) => from + i),
+      ...(to === numberOfSteps
+        ? []
+        : to === numberOfSteps - 1
+          ? [numberOfSteps - 1]
+          : [-1, numberOfSteps - 1]),
     ];
     console.log({ result });
     return result;
