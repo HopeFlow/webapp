61cea51d5c153c0bec22c959bcbba9e5f19e9dd9
Behrooz Vedadian <vedadian@gmail.com>
2025-11-04 17:02:00 +0330

    feat: Enhance Overview component layout and functionality
    Updated the Overview component to improve its layout and display of quest information. The component now includes a structured presentation of the title, share title, and reward amount, along with a conditional rendering of the cover photo. The overall styling has been enhanced for better user experience, and the button to create a quest is now integrated into the layout.
    
    - Added imports for Image and Button components.
    - Refactored the return statement to include a more detailed overview layout.
    - Included conditional rendering for the cover photo.
    - Improved text presentation for title and share title.
    
    No breaking changes.

--- src/app/(dock)/create_quest/overview.tsx
+++ src/app/(dock)/create_quest/overview.tsx
@@ -1,15 +1,46 @@
+import Image from "next/image";
 import type { InsertQuestData } from "./types";
+import { Button } from "@/components/button";
 
 export const Overview = ({
   title,
-  description,
+  shareTitle,
   rewardAmount,
   coverPhoto,
 }: {
   title: string;
+  shareTitle: string;
   description: string;
   rewardAmount: number;
   coverPhoto?: InsertQuestData["coverPhoto"];
 }) => {
-  return <div>Overview</div>;
+  return (
+    <div className="flex flex-col items-center gap-4 p-6">
+      <div className="flex max-w-2xl flex-1 flex-col gap-4 overflow-y-auto p-4">
+        <h1 className="text-2xl font-normal md:text-3xl">Overview</h1>
+        <div>
+          {coverPhoto && (
+            <Image
+              src={coverPhoto.url}
+              alt={coverPhoto.alt}
+              className="w-full h-64 w-auto rounded-md object-cover"
+              width={coverPhoto.width}
+              height={coverPhoto.height}
+            />
+          )}
+        </div>
+        <h2 className="text-md font-thin md:text-xl">
+          <span className="font-normal text-right w-48">As displayed for you</span> {title}
+        </h2>
+        <h2 className="text-md font-thin md:text-xl">
+          <span className="font-normal">As displayed for others</span>{" "}
+          {shareTitle}
+        </h2>
+        <h2 className="text-md font-normal">Reward</h2>
+        <p>{rewardAmount} Credences</p>
+      </div>
+      {/* <div className="flex-1"></div> */}
+      <Button buttonType="primary">Create Quest</Button>
+    </div>
+  );
 };
